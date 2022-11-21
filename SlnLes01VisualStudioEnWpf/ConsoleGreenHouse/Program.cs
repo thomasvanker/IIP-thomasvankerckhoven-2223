29ceadca9f278a5bdc204a1c4aaf8f3baaa5e87e
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2022-11-21 17:17:47 +0100

    ConsoleGreenHouse

---
+++ SlnLes01VisualStudioEnWpf/ConsoleGreenHouse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGreenHouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** WELKOM BIJ GREENHOUSE RESTAURANT ***");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+
+            Console.Write("Kies een grootte (kleine of grote): ");
+            string grootte = Console.ReadLine();
+            Console.Write("Kies een basis (quinoa, rijst of salade): ");
+            string basis = Console.ReadLine();
+            Console.Write("Kies een soort (vegan, zalm of kip): ");
+            string soort = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"Je bestelling: een {grootte} {basis} met {soort}");
+
+            Console.ReadLine();
+        }
+    }
+}
