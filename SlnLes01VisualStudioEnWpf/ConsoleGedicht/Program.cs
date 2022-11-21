3a5a2a41664a5211ef33bc70339f251a9f58727b
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2022-11-21 15:12:50 +0100

    ConsoleGedicht

---
+++ SlnLes01VisualStudioEnWpf/ConsoleGedicht/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGedicht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Jantje zag eens pruimen hangen,");
+            Console.ReadLine();
+
+            Console.Write("O! als eieren zo groot.");
+            Console.ReadLine();
+
+            Console.WriteLine("'t Scheen, dat Jantje wou gaan plukken,");
+            
+            Console.ReadLine();
+        }
+    }
+}
