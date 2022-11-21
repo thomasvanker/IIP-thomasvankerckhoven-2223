5b3b25156fd8df0fe6ef6966d0afcf48852272a0
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2022-11-21 14:57:56 +0100

    ConsoleHalloWereld

---
+++ SlnLes01VisualStudioEnWpf/ConsoleHalloWereld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHalloWereld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Wereld!");
+
+            Console.Write("Hoe heet je: ");
+            string naam= Console.ReadLine();
+
+            Console.Write("Aangename kennismaking, ");
+            Console.WriteLine(naam);
+
+            Console.WriteLine("druk een toets om verder te gaan...");
+
+            Console.ReadLine();
+        }
+    }
+}
