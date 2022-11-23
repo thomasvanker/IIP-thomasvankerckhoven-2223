2ece78a980eefe9b7e6c5cc762c143aa9822b76e
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2022-11-23 14:34:49 +0100

    WpfHalloWereld

---
+++ SlnLes01VisualStudioEnWpf/WpfHalloWereld/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHalloWereld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            lblHallo.Content = "Hallo " + txtName.Text;
+        }
+
+        private void btnHerstel_Click(object sender, RoutedEventArgs e)
+        {
+            lblHallo.Content = "Hallo, hoe is je naam?";
+            txtName.Text = "";
+        }
+    }
+}
