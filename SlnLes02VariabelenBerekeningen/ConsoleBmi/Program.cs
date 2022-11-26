16dd8aac9e786c982cd9ecbfd05dcfa3f6aad955
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2022-11-26 15:10:19 +0100

    ConsoleBmi

---
+++ SlnLes02VariabelenBerekeningen/ConsoleBmi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BMI CALCULATOR");
+            Console.WriteLine("==============");
+
+            Console.Write("Lengte (in cm): ");
+            int lengte1 = Convert.ToInt32(Console.ReadLine());
+            double lengte2 = Convert.ToDouble(lengte1) / Convert.ToDouble(100);
+            double lengte3 = lengte2 * lengte2;
+
+            Console.Write("Gewicht (in kg): ");
+            int gewicht = Convert.ToInt32(Console.ReadLine());
+
+            double bmi = Convert.ToDouble(gewicht) / Convert.ToDouble(lengte3);
+
+            Console.WriteLine("Je BMI bedraagt: " + Math.Round(bmi, 1));
+            Console.ReadLine();
+        }
+    }
+}
