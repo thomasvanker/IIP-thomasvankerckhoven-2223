003e3523ba155b875ea33230139d595247a01f68
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2022-11-26 16:18:23 +0100

    WpfBmi

---
+++ SlnLes02VariabelenBerekeningen/WpfBmi/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBmi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnBerekenen_Click(object sender, RoutedEventArgs e)
+        {
+            double lengte1;
+            double lengte2;
+            double lengte3;
+            double gewicht;
+            double bmi;
+            lengte1 = double.Parse(txtLengte.Text);
+            lengte2 = lengte1 / 100;
+            lengte3 = lengte2 * lengte2;
+            gewicht = double.Parse(txtGewicht.Text);
+            bmi = gewicht / lengte3;
+            lblAntw.Content = Math.Round(bmi,1);     
+        }
+    }
+}
