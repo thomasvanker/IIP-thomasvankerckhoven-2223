698549e2ac1f3b415dc51d9251bb2bac0d7b0091
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2022-12-01 13:19:56 +0100

    WpfAlcohol

---
+++ SlnLes02VariabelenBerekeningen/WpfAlcohol/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAlcohol
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sld1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblSliderBeer.Content = sld1.Value + " glazen";
+            myRectangle.Width = (sld1.Value * 25) + (sld2.Value * 25);
+            if (myRectangle.Width > yourRectangle.Width)
+            {
+                myRectangle.Fill = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                myRectangle.Fill = new SolidColorBrush(Colors.Green);
+            }
+
+        }
+
+        private void sld2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblSliderWine.Content = sld2.Value + " glazen";
+            myRectangle.Width = (sld1.Value * 25) + (sld2.Value * 25) + (sld3.Value * 25);
+            if (myRectangle.Width > yourRectangle.Width)
+            {
+                myRectangle.Fill = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                myRectangle.Fill = new SolidColorBrush(Colors.Green);
+            }
+        }
+
+        private void sld3_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblSliderWhisk.Content = sld3.Value + " glazen";
+            myRectangle.Width = (sld1.Value * 25) + (sld2.Value * 25) + (sld3.Value * 25);
+            if (myRectangle.Width > yourRectangle.Width)
+            {
+                myRectangle.Fill = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                myRectangle.Fill = new SolidColorBrush(Colors.Green);
+            }
+        }
+        
+    }
+}
--- SlnLes02VariabelenBerekeningen/WpfWinkel/MainWindow.xaml.cs
+++ SlnLes02VariabelenBerekeningen/WpfWinkel/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Schema;
 
 namespace WpfWinkel
 {
@@ -34,33 +35,49 @@ namespace WpfWinkel
             lblBanaan.Content = randomNumber;
             lblBoter.Content = randomNumber2;
             lblEggs.Content = randomNumber3;
+            
+     
         }
 
         private void btnBanaan_Click(object sender, RoutedEventArgs e)
         {
-            lstBox.Items.Add("2,20 - 1 tros bananen");
+            lstBox.Items.Add(Math.Round(2.20, 2));
             int less = Convert.ToInt32(lblBanaan.Content) - 1;
             lblBanaan.Content = less;
+            
         }
 
         private void btnBoter_Click(object sender, RoutedEventArgs e)
         {
-            lstBox.Items.Add("3,05 - 1 pak boter");
+            lstBox.Items.Add(2.75);
             int less2 = Convert.ToInt32(lblBoter.Content) - 1;
             lblBoter.Content = less2;
         }
 
         private void btnEggs_Click(object sender, RoutedEventArgs e)
         {
-            lstBox.Items.Add("2,75 - 1 doos eieren");
+            lstBox.Items.Add(3.05);
             int less3 = Convert.ToInt32(lblEggs.Content) - 1;
             lblEggs.Content = less3;
         }
 
         private void btnAfrekenen_Click(object sender, RoutedEventArgs e)
         {
+            lblTotaal.Content = lstBox.Items.OfType<double>().ToArray().Sum() + " euro";
             lstBox.Items.Clear();
 
+        }
+
+        private void btnHerstart_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(5, 50);
+            int randomNumber2 = rnd.Next(5, 50);
+            int randomNumber3 = rnd.Next(5, 50);
+            lblBanaan.Content = randomNumber;
+            lblBoter.Content = randomNumber2;
+            lblEggs.Content = randomNumber3;
+            lblTotaal.Content = "";
         }
     }
 }
