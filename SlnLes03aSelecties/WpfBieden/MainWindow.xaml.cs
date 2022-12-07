89330abfcd9b0d4dc5e7dcdc23e8829fe0b02303
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2022-12-07 15:44:39 +0100

    Woensdag

---
+++ SlnLes03aSelecties/WpfBieden/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBieden
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int hoogsteBod;
+        string hoogstebieder;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBod_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtNaam.Text;
+            string bodtext  = txtBod.Text;
+            int bod = Convert.ToInt32(bodtext);
+
+            if (bod > hoogsteBod)
+            {
+                txtHoogste.Text = $"{name} heeft met {bod} nu het hoogste bod!";
+                hoogsteBod= bod;
+                hoogstebieder = name;
+            }
+            else 
+            {
+                txtHoogste.Text = $"Sorry, {hoogstebieder} heeft momenteel het hoogste bod!";
+                
+            }
+        }
+    }
+}
---
+++ SlnLes03aSelecties/WpfFormChecking/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WpfFormChecking
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int aantalfouten = 0;
+            if (txtEmail1.Text == "")
+            {
+                lblEmail.Content =  "email moet ingevuld zijn";
+                aantalfouten++;
+            }
+
+            if (txtNaam.Text == "")
+            {
+                lblNaam.Content = "naam moet ingevuld zijn";
+                aantalfouten++;
+            }
+
+            if (txtPaswoord.Text == "")
+            {
+                lblPaswoord.Content = "kies een paswoord";
+                aantalfouten++;
+            }
+
+            if (datePicker.Text == "")
+            {
+                lblGeboorte.Content = "datum moet ingevuld zijn";
+                aantalfouten++;
+            }
+
+            if (comboBox.Text == "")
+            {
+                lblProfiel.Content = "maak een keuze";
+                aantalfouten++;
+            }
+
+
+
+
+
+            lblBevat.Content = $"dit formulier bevat {aantalfouten} fouten";
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            lblEmail.Content = "";
+            lblNaam.Content = "";
+            lblGeboorte.Content = "";
+            lblGeslacht.Content = "";
+            lblnt.Content = "";
+            lblPaswoord.Content = "";
+            lblProfiel.Content = "";
+            txtEmail1.Text = "";
+            txtNaam.Text = "";
+            txtPaswoord.Text = "";
+            lblBevat.Content = "";
+        }
+    }
+}
