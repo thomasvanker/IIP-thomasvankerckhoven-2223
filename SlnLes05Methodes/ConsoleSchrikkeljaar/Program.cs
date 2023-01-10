27e8601dadcb516125c4acf9e4caa576be3d80fb
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2023-01-10 11:52:16 +0100

    leap jaar

---
+++ SlnLes05Methodes/ConsoleSchrikkeljaar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSchrikkeljaar
+{
+    internal class Program
+    {
+        static private int SchrikkelJaar (int jaar)
+        {
+            if (jaar % 4 == 0 && jaar % 100 != 0 || jaar % 400 == 0)
+            {
+                Console.WriteLine(jaar + " is een schrikkeljaar");
+            }
+            else
+            {
+                Console.WriteLine(jaar + " is geen schrikkeljaar");
+            }
+            
+            
+
+            return jaar;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SCHRIKKELJAAR");
+            Console.WriteLine("=============");
+            while (true)
+            {
+                Console.Write("Geef een jaartal: ");
+                int jaar = Convert.ToInt32(Console.ReadLine());
+                SchrikkelJaar(jaar);
+                Console.WriteLine("");
+                if (jaar == 0 || jaar < 0)
+                {
+                    Console.WriteLine("Bedankt en tot ziens");
+                    break;
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+    
+
