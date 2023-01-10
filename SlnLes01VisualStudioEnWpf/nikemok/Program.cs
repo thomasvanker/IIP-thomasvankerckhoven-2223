2e8a306b4d2bcfdeae6862ba6807c8d3d74c39de
thomas benjamin Vankerckhoven <90988043+thomasvanker@users.noreply.github.com>
2023-01-10 09:16:56 +0100

    faculteit

---
+++ SlnLes01VisualStudioEnWpf/nikemok/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikemok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aantalKolommen = 3;
+            int aantalRijen = 2;
+            int[,] values = new int[aantalKolommen, aantalRijen];
+
+            values[0, 0] = 10;
+            values[0, 0] = 10;
+            values[0, 0] = 10;
+            values[0, 0] = 10;
+            values[0, 0] = 10;
+            values[0, 0] = 10;
+
+            for (int i = 0; i < aantalKolommen; i++)
+            {
+                Console.WriteLine(values[0, 1] + "");
+            }
+            
+            
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ SlnLes05Methodes/ConsoleFaculteit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFaculteit
+{
+    internal class Program
+    {
+      
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FACULITEIT BEREKENEN");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Geef een geheel getal: ");
+            int gegevenGetal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De faculiteit is " + Faculiteit(gegevenGetal));
+            Console.ReadKey();    
+        }
+
+        private static int Faculiteit(int n)
+        {
+
+            int getal = 1;
+
+            for (int i = 0; i < n; i++) 
+            {
+                getal += getal * i;
+            }
+
+
+            return getal;
+
+        }
+
+        
+    }
+}
